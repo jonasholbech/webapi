7c289e82f3fb9da294d6dc488c429eed73633438
Jonas Holbech <jh@jonasholbech.dk>
2016-06-26 09:53:57 +0200

    debugging for touchMove

--- js/touch.js
+++ js/touch.js
@@ -2,25 +2,37 @@
  * Created by holbech on 08/06/16.
  */
 var touchCountElem = document.createElement('p'),
-    touchTypeElem = document.createElement('p');
+    touchTypeElem = document.createElement('p'),
+    touchDebugElem = document.createElement('p');
 
-function handleTouchEvent(e){
+function handleTouchCancel(e){
+    e.preventDefault();
+    touchTypeElem.innerHTML=e.type;
+}
+function handleTouchMove(e){
+    e.preventDefault();
+    touchTypeElem.innerHTML=e.type;
+    touchDebugElem.innerHTML = e.touches[0].pageX + " " + e.touches[0].pageY;
+}
+function handleTouchEnd(e){
+    e.preventDefault();
+    touchTypeElem.innerHTML=e.type;
+}
+function handleTouchStart(e){
+    e.preventDefault();
     var allTouches= e.touches,
         allTouchesLength=allTouches.length;
-    if(e.type==="touchstart"){
-        e.preventDefault();
-    }
-
     touchCountElem.innerHTML="There are currently "+allTouchesLength+" touches on the screen";
     touchTypeElem.innerHTML=e.type;
 }
 document.body.appendChild(touchCountElem);
 document.body.appendChild(touchTypeElem);
+document.body.appendChild(touchDebugElem);
 
-window.addEventListener('touchstart', handleTouchEvent, false);
-window.addEventListener('touchend', handleTouchEvent, false);
-window.addEventListener('touchcancel', handleTouchEvent, false);
-window.addEventListener('touchmove', handleTouchEvent, false);
+window.addEventListener('touchstart', handleTouchStart, false);
+window.addEventListener('touchend', handleTouchEnd, false);
+window.addEventListener('touchcancel', handleTouchCancel, false);
+window.addEventListener('touchmove', handleTouchMove, false);
 
 
 
