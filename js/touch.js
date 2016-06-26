759779c0ddef342dea415f61f85e0d0ccd8fedff
Jonas Holbech <jh@jonasholbech.dk>
2016-06-26 14:28:50 +0200

    multiple paths possible, real-time draw

--- js/touch.js
+++ js/touch.js
@@ -5,7 +5,8 @@ var touchCountElem = document.createElement('p'),
     touchTypeElem = document.createElement('p'),
     touchDebugElem = document.createElement('p'),
     svg = document.querySelector('svg'),
-    coords=[];
+    coords=[],
+    paths=[];
 
 function handleTouchCancel(e){
     e.preventDefault();
@@ -17,20 +18,27 @@ function handleTouchMove(e){
     touchTypeElem.innerHTML=e.type;
     touchDebugElem.innerHTML = e.touches[0].pageX + " " + e.touches[0].pageY;
     coords.push([e.touches[0].pageX, e.touches[0].pageY]);
+
+    var i;
+    //var t = coords.shift();
+    var dString="M ";
+    for(i=0; i<coords.length; i++){
+        dString+=coords[i][0]+" "+coords[i][1]+" ";
+    }
+    paths[paths.length-1].setAttribute('d', dString);
 }
 function handleTouchEnd(e){
     e.preventDefault();
     touchTypeElem.innerHTML=e.type;
-    var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'path'); //Create a path in SVG's namespace
+
 
     var i;
-    var t = coords.shift();
-    var dString="M "+t[0]+ " " + t[1] + " ";
+    //var t = coords.shift();
+    var dString="M ";
     for(i=0; i<coords.length; i++){
         dString+=coords[i][0]+" "+coords[i][1]+" ";
     }
-    newElement.setAttribute("d",dString); //Set path's data
-    svg.appendChild(newElement);
+    paths[paths.length-1].setAttribute('d', dString);
     coords=[];
 }
 function handleTouchStart(e){
@@ -41,6 +49,10 @@ function handleTouchStart(e){
     touchTypeElem.innerHTML=e.type;
 
     coords.push([e.touches[0].pageX, e.touches[0].pageY]);
+    var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'path'); //Create a path in SVG's namespace
+    newElement.setAttribute("d",dString); //Set path's data
+    svg.appendChild(newElement);
+    paths.push(newElement)
     /*
     var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'path'); //Create a path in SVG's namespace
     newElement.setAttribute("d","M 0 0 L 10 10"); //Set path's data
