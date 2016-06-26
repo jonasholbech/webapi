0dd7b31a7106b0d933033425ae642c9ab5bace74
Jonas Holbech <jh@jonasholbech.dk>
2016-06-26 16:51:47 +0200

    switch to o'clock method

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -2,6 +2,7 @@
  * Created by holbech on 26/06/16.
  */
 
+/*
 var body = document.querySelector('body');
 var button = document.createElement('button');
 button.appendChild(document.createTextNode("Toggle fullscreen"));
@@ -22,3 +23,23 @@ function launchIntoFullscreen() {
 }
 button.addEventListener('click', launchIntoFullscreen, false);
 document.body.appendChild(button);
+*/
+document.onclick = function (argument) {
+    var conf = confirm("Fullscreen mode?");
+    var docelem = document.documentElement;
+
+    if (conf == true) {
+        if (docelem.requestFullscreen) {
+            docelem.requestFullscreen();
+        }
+        else if (docelem.mozRequestFullScreen) {
+            docelem.mozRequestFullScreen();
+        }
+        else if (docelem.webkitRequestFullScreen) {
+            docelem.webkitRequestFullScreen();
+        }
+        else if (docelem.msRequestFullscreen) {
+            docelem.msRequestFullscreen();
+        }
+    }
+}
