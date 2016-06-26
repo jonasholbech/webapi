181b024eb6c7f88f02a81ea578816e1017a0aaa2
Jonas Holbech <jh@jonasholbech.dk>
2016-06-26 15:24:50 +0200

    add a ball to visualise beta/gamma

--- js/deviceorientation.js
+++ js/deviceorientation.js
@@ -2,13 +2,24 @@
  * Created by holbech on 26/06/16.
  */
 
-var debugElem = document.createElement('p');
+var debugElem = document.createElement('p'),
+    ball = document.createElement('div');
+ball.style.backgroundColor="red";
+ball.style.borderRadius="50%";
+ball.style.width=ball.style.height="80px";
+ball.style.position="absolute";
+ballPosition={x:0, y:0}
 function handleOrientationEvent(e){
-    var alpha = e.alpha,
-        beta = e.beta,
-        gamma = e.gamma;
-    debugElem.innerHTML= "alpha: "+ alpha + "<br>beta: " + beta+ "<br>gamma: " + gamma;
+    var //alpha = e.alpha,
+        beta = e.beta,//front to back
+        gamma = e.gamma;//left to right
+    debugElem.innerHTML= "beta: " + beta+ "<br>gamma: " + gamma;
+    ballPosition.x+=beta;
+    ballPosition.y+=gamma;
+    ball.style.left=ballPosition.x+"px";
+    ball.style.top= ballPosition.y+"px";
 }
 
 document.body.appendChild(debugElem);
+document.body.appendChild(ball);
 window.addEventListener('deviceorientation', handleOrientationEvent, false);
