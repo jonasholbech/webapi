08f2154451be7bde6c0bc3b57574655659a22329
Jonas Holbech <jh@jonasholbech.dk>
2016-06-26 21:59:22 +0200

    add documentation/note

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -2,28 +2,7 @@
  * Created by holbech on 26/06/16.
  */
 
-/*
-var body = document.querySelector('body');
-var button = document.createElement('button');
-button.appendChild(document.createTextNode("Toggle fullscreen"));
-function launchIntoFullscreen() {
-    var docelem = document.documentElement;
-    if (docelem.requestFullscreen) {
-        docelem.requestFullscreen();
-    }
-    else if (docelem.mozRequestFullScreen) {
-        docelem.mozRequestFullScreen();
-    }
-    else if (docelem.webkitRequestFullscreen) {
-        docelem.webkitRequestFullscreen();
-    }
-    else if (docelem.msRequestFullscreen) {
-        docelem.msRequestFullscreen();
-    }
-}
-button.addEventListener('click', launchIntoFullscreen, false);
-document.body.appendChild(button);
-*/
+
 document.onclick = function (argument) {
     var doc = window.document;
     var docEl = doc.documentElement;
