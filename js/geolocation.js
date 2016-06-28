ba6b61bbc005f6b0ed8528fe035b6cac78baf42f
Jonas Holbech <jh@jonasholbech.dk>
2016-06-28 13:16:45 +0200

    no message

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,82 +1,33 @@
-/**
- * Created by holbech on 28/06/16.
- */
-var positionFound=false;
-var debugElem = document.createElement('p');
-document.body.appendChild(debugElem);
-var crds, googleReady=false;
-Math.toRadians = function(degrees) {
-    return degrees * Math.PI / 180;
-};
-function logIt(){
-    debugElem.innerHTML=arguments.join(" ");
-}
-function getDistance(lat1, lon1, lat2, lon2){
-    var R = 6371000; // metres
-    var φ1 = Math.toRadians(lat1);
-    var φ2 = Math.toRadians(lat2);
-    var Δφ = Math.toRadians(lat2-lat1);
-    var Δλ = Math.toRadians(lon2-lon1);
-
-    var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-        Math.cos(φ1) * Math.cos(φ2) *
-        Math.sin(Δλ/2) * Math.sin(Δλ/2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
-    return R * c/1000;//Kilometers
-}
-/*
-var watchID = navigator.geolocation.watchPosition(function(position) {
-    positionFound=true;
-    console.log(position.coords.latitude, position.coords.longitude);
-
-    debugElem.innerHTML=updates+": "+position.coords.latitude +" "+ position.coords.longitude;
-    //console.log(
-     //getDistance(
-     //position.coords.latitude,
-     //position.coords.longitude,
-     //55.706353,
-     //12.538980
-     //)
-     //);
-});*/
-
-var options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-};
-
-var map;
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: crds.latitude, lng: crds.longitude},
-        zoom: 24
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 6
     });
-}
+    var infoWindow = new google.maps.InfoWindow({map: map});
 
-function setReady(){
-    googleReady=true;
-    start();
-}
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
 
-function start(){
-    if(googleReady && crds){
-        initMap();
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
     }
 }
-function success(pos) {
-    crds = pos.coords;
 
-    console.log('Your current position is:');
-    console.log('Latitude : ' + crds.latitude);
-    console.log('Longitude: ' + crds.longitude);
-    console.log('More or less ' + crds.accuracy + ' meters.');
-    start();
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
 }
-
-function error(err) {
-    console.warn('ERROR(' + err.code + '): ' + err.message);
-}
-
-navigator.geolocation.getCurrentPosition(success, error, options);
