978df4f930e7843abf22837d2669203488e90ea7
= <jh@jonasholbech.dk>
2016-08-29 22:40:34 +0200

    clean up

--- js/fetch.js
+++ js/fetch.js
@@ -3,50 +3,51 @@
  */
 (function(){
     "use strict";
-var template = {};
-function init() {
-    template.content = document.querySelector("#templateTechnologies").content;
-    template.header = template.content.querySelector('h1');
-    template.container = document.querySelector('body');
-    getJSON('data/data.json', 'json', show);
+    var template = {};
+    function init() {
+        template.content = document.querySelector("#templateTechnologies").content;
+        template.header = template.content.querySelector('h1');
+        template.container = document.querySelector('body');
+        getJSON('data/data.json', 'json', show);
 
-}
+    }
 
-function getJSON(what, type, callback){
-    fetch(what)
-        .then(function (response) {
-            return response[type]();
-        })
-        .then(function(data){
-            callback(data)
+    function getJSON(what, type, callback){
+        fetch(what)
+            .then(function (response) {
+                return response[type]();
+            })
+            .then(function(data){
+                callback(data)
+            });
+    }
+
+
+
+    function show(data){
+        console.log(data);
+        data.items.forEach(function(item){
+            console.log(item);
+            template.header.textContent = item.name;
+            var clone = document.importNode(template.content, true);
+            template.container.appendChild(clone);
         });
-}
+    }
 
+    if(!('fetch' in window)){
+        console.log("loading polyfills");
+        var s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.src = 'js/polyfills/fetch.polyfill.js';
+        var x = document.getElementsByTagName('script')[0];
+        x.parentNode.insertBefore(s, x);
+        s.onload = function(){
+            console.log("script should be loaded");
+            init();
+        };
 
-
-function show(data){
-    console.log(data);
-    data.items.forEach(function(item){
-        console.log(item);
-        template.header.textContent = item.name;
-        var clone = document.importNode(template.content, true);
-        template.container.appendChild(clone);
-    });
-}
-if(!('fetch' in window)){
-    console.log("loading polyfill");
-    var s = document.createElement('script');
-    s.type = 'text/javascript';
-    s.src = 'js/polyfills/fetch.polyfill.js';
-    var x = document.getElementsByTagName('script')[0];
-    x.parentNode.insertBefore(s, x);
-    s.onload = function(){
-        console.log("script should be loaded");
-        init();
-    };
-
-} else {
-    console.log("DOMContentLoaded ramt");
-    document.addEventListener('DOMContentLoaded', init);
-}
+    } else {
+        console.log("DOMContentLoaded ramt");
+        document.addEventListener('DOMContentLoaded', init);
+    }
 })();
