e6e9a189a7fccff4fbc680936bf1b34839ae5e3d
= <jh@jonasholbech.dk>
2016-09-22 09:19:11 +0200

    comment out audio

--- js/webRTC.js
+++ js/webRTC.js
@@ -17,8 +17,8 @@ document.getElementById('flip-button').onclick = function() { front = !front; };
 
 if(navigator.getUserMedia){
     navigator.getUserMedia({
-        video: { facingMode: (front? "user" : "environment") },
-        audio:true
+        video: { facingMode: (front? "user" : "environment") }/*,
+        audio:true*/
     }, onSuccess, onError);
 } else {
     throw new Error("Sorry, getUserMedia is not supported in your browser");
