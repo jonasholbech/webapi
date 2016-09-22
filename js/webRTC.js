e5ba71e64a7829191705ac919bd7cdd03cf38c9c
= <jh@jonasholbech.dk>
2016-09-22 09:51:46 +0200

    list devices

--- js/webRTC.js
+++ js/webRTC.js
@@ -1,6 +1,9 @@
 navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia || navigator.msGetUserMedia;
 
 function onSuccess(stream){
+    if(var vid=document.querySelector('video')){
+        document.removeChild(vid);
+    }
     var video = document.createElement('video'),
     videoSource = window.URL.createObjectURL(stream);
     video.autoplay=true;
@@ -17,13 +20,29 @@ document.getElementById('flip-button').onclick = function() {
     front = !front;
     if(navigator.getUserMedia){
         navigator.getUserMedia({
-            video: { facingMode: (front? "user" : "environment") }/*,
+            video: { facingMode: (front ? "user" : "environment") }/*,
              audio:true*/
         }, onSuccess, onError);
     } else {
         throw new Error("Sorry, getUserMedia is not supported in your browser");
     }
 };
+
+var ul = document.createElement('ul');
+
+navigator.mediaDevices.enumerateDevices()
+    .then(function(devices) {
+        devices.forEach(function(device) {
+            let li = document.createElement('li');
+            li.innerHTML = device.kind + ": " + device.label +
+                " id = " + device.deviceId;
+            ul.appendChild(li)
+        });
+        document.body.appendChild(ul);
+    })
+    .catch(function(err) {
+        console.log(err.name + ": " + err.message);
+    });
 
 
 
